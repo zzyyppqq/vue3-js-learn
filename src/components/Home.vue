3572ad6cb98b1f76478f9f895c7535a4f08bafa0
zzyyppqq <zzyyppqq@gmail.com>
2024-11-15 14:28:29 +0800

    init vue3 + vite项目

---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<time date></time>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+// 从一个单文件组件中导入根组件
+import App from './App.vue'
+
+const app = createApp(App)
+// 应用实例会暴露一个 .config 对象允许我们配置一些应用级的选项
+// 例如定义一个应用级的错误处理器，用来捕获所有子组件上的错误：
+app.config.errorHandler = (err) => {
+
+}
+// .mount() 方法应该始终在整个应用配置和资源注册完成后被调用
+app.mount('#app')
+
+// 多个应用实例
+// const app2 = createApp({
+//     /* ... */
+// })
+// app2.mount('#container-2')
